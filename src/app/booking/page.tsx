ca422d25c4b9a78c71d92bdc2d86e259fd62b841
Nail Badiullin <nbadiullin@provectus.com>
2023-04-17 14:30:43 +0400

    feature/segment add generic segment track messages

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -1,9 +1,12 @@
 import { Form } from "@/components/ui/form";
+import { SegmentClient } from "@/components/ui/segment-client";
 
 export default function Booking() {
   return (
-    <div className="flex h-full items-center justify-center">
-      <Form />
-    </div>
+    <SegmentClient>
+      <div className="flex h-full items-center justify-center">
+        <Form />
+      </div>
+    </SegmentClient>
   );
 }
